f92732bd5a0c275ced6df099723c355e2348369c
Fidel Yin <fidel.yin@hotmail.com>
2026-05-03 15:07:58 +0800

    perf(unicode): simplify unicode input pattern recognizing

--- lua/wanxiang/unicode.lua
+++ lua/wanxiang/unicode.lua
@@ -2,54 +2,16 @@
 ---@author amzxyz
 ---@author Fidel Yin <fidel.yin@hotmail.com>
 
----@class UnicodeConfig
----@field trigger string
-
----@diagnostic disable-next-line: duplicate-type
----@class Env
----@field unicode_config UnicodeConfig?
-
-local T = {}
-
----@param env Env
-function T.init(env)
-    -- Extract the second character of the configured trigger as the trigger key.
-    local pattern = env.engine.schema.config:get_string("recognizer/patterns/unicode")
-    local trigger = pattern and pattern:sub(2, 2) or ""
-
-    env.unicode_config = {
-        trigger = trigger,
-    }
-end
-
----@param env Env
-function T.fini(env)
-    env.unicode_config = nil
-end
-
 ---@param input string
----@param seg Segment
----@param env Env
-function T.func(input, seg, env)
-    local config = env.unicode_config
-    assert(config)
-
-    if not seg:has_tag("unicode") or config.trigger == "" then
-        return
-    end
-    if input:sub(1, 1) ~= config.trigger then
+---@param segment Segment
+function translator(input, segment, _)
+    if not segment:has_tag("unicode") then
         return
     end
 
-    local ucodestr = input:match(config.trigger .. "(%x+)")
-    if not ucodestr or #ucodestr <= 1 then
+    local ucodestr = input:sub(2)
+    if #ucodestr <= 1 then
         return
-    end
-
-    local segment = env.engine.context.composition:back()
-    if segment then
-        ---@diagnostic disable-next-line: assign-type-mismatch
-        segment.tags = segment.tags + Set({ "unicode" })
     end
 
     local code = tonumber(ucodestr, 16)
@@ -68,7 +30,7 @@ function T.func(input, seg, env)
     end
 
     local text = utf8.char(code)
-    yield(Candidate("unicode", seg.start, seg._end, text, ("U%x"):format(code)))
+    yield(Candidate("unicode", segment.start, segment._end, text, ("U%x"):format(code):upper()))
 
     if code < 0x10000 then
         for i = 0, 15 do
@@ -80,11 +42,11 @@ function T.func(input, seg, env)
             end
 
             text = utf8.char(new_code)
-            yield(Candidate("unicode", seg.start, seg._end, text, ("U%x~%x"):format(code, i)))
+            yield(Candidate("unicode", segment.start, segment._end, text, ("U%x~%x"):format(code, i):upper()))
 
             ::continue::
         end
     end
 end
 
-return T
+return translator
